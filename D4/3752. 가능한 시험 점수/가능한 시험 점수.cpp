aadc17c78c7cc4f75d34a8a85225905823ea8964
YeEun LEE <121779892+mindal1@users.noreply.github.com>
2025-09-25 23:20:02 +0900

    Add files via upload

---
+++ D4/3752. 가능한 시험 점수/가능한 시험 점수.cpp
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+
+{
+	int T = 0;	// TestCase(배점 가능성)
+	int N = 0;	// 배점 개수
+	int total_sum = 0;
+	int possible[10001];	// 나올 수 있는 모든 배점 값
+	int count = 0;	// 나올 수 있는 배점 개수
+
+
+	int scores[101];
+	int* result;
+
+	scanf("%d", &T);
+	//Testcase T번 돌리기 결정
+	result = (int*)malloc(sizeof(int) * T);
+
+	for (int i = 0; i < T; i++)
+	{
+		memset(possible, 0, sizeof(possible));
+
+		scanf("%d", &N);
+
+		for (int j = 0; j < N; j++)
+		{
+			scanf("%d", &scores[j]);
+			total_sum += scores[j];
+		}
+
+		possible[0] = 1;
+		// 인덱스는 점수를 뜻함. 0은 나올 수 있는 배점이라는 뜻(1=true)
+
+		for (int x = 0; x < N; x++)
+		{
+			for (int y = total_sum; y >= 0; y--)
+			{
+				if (possible[y] == 1)
+					possible[(y + scores[x])] = 1;
+				// 가능한 배점에 입력한 배점을 더한 값은 나올 수 있는 배점이다.
+			}
+		}
+
+		for (int c = 0; c <= total_sum; c++)
+		{
+			if (possible[c] == 1)
+				count++;
+		}
+		result[i] = count;
+		count = 0;
+	}
+
+	for (int i = 0; i < T; i++)
+	{
+		printf("#%d %d\n", i + 1, result[i]);
+	}
+
+
+	return 0;
+}
